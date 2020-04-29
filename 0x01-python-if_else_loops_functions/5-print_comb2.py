eefc015f9f1da74177c289ab76516e9135674865
jesgogu27 <jesgogu@hotmail.com>
2020-04-28 21:37:38 -0500

    5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 100):
     if i != 99:
-        print("{:d}, ".format(i), end="")
+        if i >= 0 and i <= 9:
+            print("0{:d}, ".format(i), end="")
+        else:
+            print("{:d}, ".format(i), end="")
     else:
-        print("{:d}".format(i))
+        print("{:d} ".format(i))
