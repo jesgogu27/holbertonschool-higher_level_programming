4499871491acf4d44b3be1986fa6a9ffbaa6fe9d
jesgogu27 <jesgogu@hotmail.com>
2020-05-18 16:08:16 -0500

    delete pep8 error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     j = 0
     for i in range(x):
         try:
-            print('{}'.format(my_list[i]), end ="")
+            print('{}'.format(my_list[i]), end="")
             j = j + 1
         except IndexError:
-        	break
+            break
     print()
     return j
--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:    	
-        if value >= 0 or value <= 0 :
+    try:
+        if value >= 0 or value <= 0:
             print("{:d}".format(value))
             return True
     except:
