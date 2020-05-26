88b9ab188ecae8dcc4c2e471f0592e75e60073d6
jesgogu27 <jesgogu@hotmail.com>
2020-05-25 21:41:43 -0500

    repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -61,4 +61,4 @@ class Rectangle:
 
     def __repr__(self):
         """printed rep"""
-        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height))
+        return ("Rectangle({:d}, {:d})".format(self.__width, self.__height)0)
