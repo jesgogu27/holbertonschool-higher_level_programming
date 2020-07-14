ff3ca1854448f993310e447d46e2bf922a4a9114
jesgogu27 <jesgogu@hotmail.com>
2020-07-14 16:00:20 -0500

    provilegios

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- Show Privilegies that all users.
-SHOW GRANTS FOR user_0d_1 @localhost;
-SHOW GRANTS FOR user_0d_2 @localhost;
+SHOW GRANTS 
+FOR user_0d_1@localhost;
+SHOW GRANTS 
+FOR user_0d_2@localhost;
