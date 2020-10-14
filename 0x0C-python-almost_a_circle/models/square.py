e793e32b9a0d12d5e4bf2e66214368fc64d0b68a
jesgogu27 <jesgogu@hotmail.com>
2020-10-13 22:08:09 -0500

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -28,3 +28,34 @@ class Square(Rectangle):
         '''setter for size attribute'''
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        '''asign attributes'''
+        if args:
+            for arg in range(len(args)):
+                if arg == 0:
+                    self.id = args[arg]
+                if arg == 1:
+                    self.size = args[arg]
+                if arg == 2:
+                    self.x = args[arg]
+                if arg == 3:
+                    self.y = args[arg]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
+
+    def to_dictionary(self):
+        '''return a dictionary of a square'''
+        new_dict = {}
+        new_dict['id'] = self.id
+        new_dict['size'] = self.size
+        new_dict['x'] = self.x
+        new_dict['y'] = self.y
+        return new_dict
