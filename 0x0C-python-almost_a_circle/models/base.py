ca3b801b558c594b57a3918951808ac4281393fa
jesgogu27 <jesgogu@hotmail.com>
2020-10-13 22:18:11 -0500

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,22 +1,3 @@
-#!/usr/bin/python3
-'''Base module'''
-
-
-import json
-
-
-class Base:
-    '''class Base'''
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        '''initialization contructor with attributes'''
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects = Base.__nb_objects + 1
-            self.id = Base.__nb_objects
-
     @staticmethod
     def to_json_string(list_dictionaries):
         '''return of dictionary to JSON string'''
