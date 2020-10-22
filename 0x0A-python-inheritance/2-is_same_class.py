b7d333590f76ad714b7df7d587c69a120d33a618
jesgogu27 <jesgogu@hotmail.com>
2020-10-21 23:20:01 -0500

    isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,16 @@
 #!/usr/bin/python3
+
+"""
+ is true o false
+"""
+
 def is_same_class(obj, a_class):
-	r = isinstance(obj, a_class)
-	return r
+
+    """
+    is_same_class - return true o false
+    obj - object
+    a_class - class
+    """
+
+    r = isinstance(obj, a_class)
+    return r
