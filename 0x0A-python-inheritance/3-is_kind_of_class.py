d26e298b66ff0430d592988f983fffd509a0a9b4
jesgogu27 <jesgogu@hotmail.com>
2020-10-21 23:45:48 -0500

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+ is true o false
+"""
+
+
+def is_kind_of_class(obj, a_class):
+
+    """
+    is_same_class - return true o false
+    obj - object
+    a_class - class
+    """
+
+    r = isinstance(obj, a_class)
+    return r
