9e8af05f8659d2039c799d1e3badf6e16d9798f7
jesgogu27 <jesgogu@hotmail.com>
2020-11-19 23:22:48 -0500

    exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+   Class BaseGeometry
+"""
+
+
+class BaseGeometry:
+
+    """
+        BaseGeometry
+    """
+
+    def area(self):
+        """
+        area - return a msj Exception
+        """
+        raise Exception("area() is not implemented")
