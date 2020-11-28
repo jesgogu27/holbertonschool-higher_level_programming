e4c1536c6acfda7713fc1beaf7f023f73b94d8f6
jesgogu27 <jesgogu@hotmail.com>
2020-11-27 21:12:42 -0500

    print square

--- 0x07-python-test_driven_development/4-main.py
+++ 0x07-python-test_driven_development/4-main.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
+print_square = __import__('4-print_square').print_square
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
+print_square(4)
+print("")
+print_square(10)
+print("")
+print_square(0)
+print("")
+print_square(1)
+print("")
+try:
+    print_square(-1)
+except Exception as e:
+    print(e)
+print("")
