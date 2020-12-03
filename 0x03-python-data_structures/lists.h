14ff894837342e284d955552fccf73911201a9cf
jesgogu27 <jesgogu@hotmail.com>
2020-12-02 23:53:59 -0500

    is palindrome

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,18 +1,21 @@
 #ifndef LISTS_H
 #define LISTS_H
+
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * for Holberton project
- */
+   * struct listint_s - singly linked list
+    * @n: integer
+	 * @next: points to the next node
+	  *
+	   * Description: singly linked list node structure
+	    * for Holberton project
+		 */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+	    int n;
+		    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -21,4 +24,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif /* LISTS_H */
+#endif
